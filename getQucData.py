506754b1cbb736dcfdf51a86d3aa0517d87f0d89
Carlos Cardoso <carlosjccardoso@tecnico.ulisboa.pt>
2022-05-17 09:45:32 +0100

    Refactored most 'get' functions, added main function and added error detection

--- getQucData.py
+++ getQucData.py
@@ -3,13 +3,6 @@ from bs4 import BeautifulSoup
 from parseScriptQUC import getScriptAnswer
 
 
-
-url = 'https://fenix.tecnico.ulisboa.pt/publico/viewCourseResults.do?executionCourseID=1690460473010771&degreeCurricularPlanOID=2581275345334'
-r = requests.get(url)
-
-soup = BeautifulSoup(r.text, 'html.parser')
-tables = soup.findAll("table", { "class" : "graph neutral table" })
-
 def barcolorToText(barcolor):
     # Verde   - De acordo com o previsto
     # Amarelo - Acima do previsto
@@ -28,15 +21,58 @@ def barcolorToText(barcolor):
 
 # Resultados Gerais
 def getGeneralResults(soup):
+    graph_dict = {}
     table = soup.findAll("table", { "class" : "graph general-results table" })
-    tds = table[0].findAll("td")
+    if table:
+        tds = table[0].findAll("td")
+        # Get table headers
+        ths = table[0].findAll("th")
+        if tds:
+            for i in range(0, len(tds)):
+                header = ths[i].contents[0].strip()
+                value = barcolorToText(tds[i].div['class'][0])
+                graph_dict = graph_dict | {header : value}
 
-    graph_dict = {'Carga de trabalho': barcolorToText(tds[0].div['class'][0]), 
-                  'Organização da UC': barcolorToText(tds[1].div['class'][0]), 
-                  'Avaliação da UC'  : barcolorToText(tds[2].div['class'][0]), 
-                  'Docência da UC'   : barcolorToText(tds[3].div['class'][0])}
+            return {'Resultados gerais da UC e estatísticas de preenchimento' : graph_dict}
+        else:
+            return {}
+    else:
+        return {}
+        
 
-    return {'Resultados gerais da UC e estatísticas de preenchimento' : graph_dict}
+def parseGraphTable(table):
+    info = {}
+    
+    td_divs = table.findAll('div', {"class": "graph-bar-horz-number"})
+    ths = table.findAll('th')
+
+    if td_divs and ths:
+        # "Nº de respostas: 12"
+        answers = ths[0].text.split(":") 
+
+        info = info | {answers[0].strip() : answers[1].strip()}
+
+        # 'O programa não foi cumprido' : '0%'        
+        for i in range(0, len(td_divs)):
+            header = ths[i+1].text.strip()
+            value = td_divs[i].text.strip()
+            info = info | {header : value}
+        
+    return info
+
+def parseGraph2colTable(table):
+    graph2col_dict = {}
+    if table:
+        trs = table.findAll("tr")
+        ths = table.findAll("th")
+        if trs and ths:
+            for i in range (0, len(trs)):
+                header = ths[i].text.strip()
+                value  = trs[i].td.text.strip()
+                graph2col_dict = graph2col_dict | {header : value}
+
+    return graph2col_dict
+
 
 # 1. Acompanhamento da UC ao longo do semestre/carga de trabalho da UC
 # Para os alunos que indicaram acréscimo de carga de trabalho, a razão deveu-se a:
@@ -44,45 +80,29 @@ def getGeneralResults(soup):
 def getAttendance(soup):
     tables = soup.findAll("table", { "class" : "graph table" })
 
-    table_high = tables[0]
-    td_divs = table_high.findAll('div', {"class": "graph-bar-horz-number"})
-    thigh_dict = {'Trabalhos/projectos complexos'                             : td_divs[0].text, 
-                  'Trabalhos/projectos extensos'                              : td_divs[1].text, 
-                  'Trabalhos/projectos em número elevado'                     : td_divs[2].text, 
-                  'Falta de preparação anterior exigindo mais trabalho/estudo': td_divs[3].text,
-                  'Extensão do programa face ao nº de aulas previstas'	 	  : td_divs[4].text,
-                  'Pouco acompanhamento das aulas ao longo do semestre'       : td_divs[5].text,
-                  'Problemas na organização da UC'	                          : td_divs[6].text,
-                  'Problemas pessoais / com os colegas de grupo'              : td_divs[7].text,
-                  'Outras razões'                                             : td_divs[8].text,
-                  }
+    if (len(tables) == 2):
+        table_high = tables[0]
+        thigh_dict = parseGraphTable(table_high)
+   
+        table_down = tables[1]
+        tdown_dict = parseGraphTable(table_down)
 
-    table_down = tables[1]
-    td_divs = table_down.findAll('div', {"class": "graph-bar-horz-number"})
-    tdown_dict = {'O programa não foi cumprido'                               : td_divs[0].text, 
-                  'O programa foi pouco extenso'                              : td_divs[1].text, 
-                  'A matéria já tinha sido leccionada noutra UC'              : td_divs[2].text,
-                  'Frequência de UC em repetência'                            : td_divs[3].text,
-                  'Trabalho excessivo noutras UC'	 	                      : td_divs[4].text,
-                  'Avaliação pouco exigente'                                  : td_divs[5].text,
-                  'Boa organização da UC'	                                  : td_divs[6].text,
-                  'Outras razões:'                                            : td_divs[7].text, 
-                  }
-
-    both = {"Para os alunos que indicaram acréscimo de carga de trabalho, a razão deveu-se a" : thigh_dict,
-            "Para os alunos que indicaram baixa carga de trabalho, a razão deveu-se a:"       : tdown_dict }
+        both = {"Para os alunos que indicaram acréscimo de carga de trabalho, a razão deveu-se a" : thigh_dict,
+                "Para os alunos que indicaram baixa carga de trabalho, a razão deveu-se a:"       : tdown_dict }
     
-    return {'1. Acompanhamento da UC ao longo do semestre/carga de trabalho da UC' : both}
+        return {'1. Acompanhamento da UC ao longo do semestre/carga de trabalho da UC' : both}
+    else:
+        return {}
 
 def parseNeutralTableHeaders(neutralTable):
     headers = []
     head = neutralTable.findAll("tr", {"class" : "thead"})
-    ths = head[0].findAll("th")
-
-    # Ignore first and last th
-    for i in range(1, len(ths) -1):
-        headers.append(ths[i].text.strip())
-
+    if head:
+        ths = head[0].findAll("th")
+        if ths:
+            # Ignore first and last th
+            for i in range(1, len(ths) -1):
+                headers.append(ths[i].text.strip())
     return headers
 
 def parseNeutralTableRow(row, headers):
@@ -128,13 +148,14 @@ def getKnowledge(soup):
     tables = soup.findAll("table", { "class" : "graph neutral table" })
     table  = tables[0]
 
-    rows = table.findAll("tr")
-    headers = parseNeutralTableHeaders(table)
+    if table:
+        rows = table.findAll("tr")
+        headers = parseNeutralTableHeaders(table)
 
-    info = parseNeutralTableRow(rows[2], headers)
-    
-    knowledge = {"1.2 Os conhecimentos anteriores foram suficientes para o acompanhamento desta UC": info}
-    #print(knowledge)
+        info = parseNeutralTableRow(rows[2], headers)
+        
+        knowledge = {"1.2 Os conhecimentos anteriores foram suficientes para o acompanhamento desta UC": info}
+        #print(knowledge)
     return knowledge
 
 # 1.3 Caracterização do nível de importância que atribui aos meios de estudos, quando utilizados, nesta UC:
@@ -157,19 +178,13 @@ def getOrganization(soup):
 
     return {'2. Organização da UC' : organization}
 
-
 # 3. Método de avaliação da UC
 def getEvaluationMethod(soup):
     evaluation = {}
 
+    # Get graph-2col table and parse
     table = soup.findAll("table", { "class" : "graph-2col" })
-    trs = table[0].findAll("tr")
-    graph2col_dict = {'Nº de inscritos'   : trs[0].td.text.strip(), 
-                      'Nº de aprovados'   : trs[1].td.text.strip(), 
-                      'Taxa de aprovação' : trs[2].td.text.strip(), 
-                      'Tx. de aprovação média no ano curricular do curso':  trs[3].td.text.strip(),
-                      'Média classificações': trs[4].td.text.strip()}
-    #print(graph2col_dict)
+    graph2col_dict = parseGraph2colTable(table[0])
 
     # Get Classification Table
     tables = soup.findAll("table", { "class" : "graph classification table" })
@@ -194,16 +209,32 @@ def getDevelopment(soup):
     return {'4. A UC contribuiu para a aquisição e/ou desenvolvimento das competências' : development}
 
 
-answersQUC = getScriptAnswer(soup)
-answersQUC['General Results'] = getGeneralResults(soup)     #0
-answersQUC['Attendance'] = getAttendance(soup)              #1.1
-answersQUC['Previous Knowledge'] = getKnowledge(soup)       #1.2
-answersQUC['Importance'] = getImportance(soup)              #1.3
-answersQUC['Organization'] = getOrganization(soup)          #2
-answersQUC['Evaluation Method'] = getEvaluationMethod(soup) #3
-answersQUC['Development'] = getDevelopment(soup)            #4
+def getQUCdata(url):
+    try:
+        r = requests.get(url)
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)
+    else:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        answersQUC = getScriptAnswer(soup)
+        answersQUC['General Results'] = getGeneralResults(soup)     #0
+        answersQUC['Attendance'] = getAttendance(soup)              #1.1
+        answersQUC['Previous Knowledge'] = getKnowledge(soup)       #1.2
+        answersQUC['Importance'] = getImportance(soup)              #1.3
+        answersQUC['Organization'] = getOrganization(soup)          #2
+        answersQUC['Evaluation Method'] = getEvaluationMethod(soup) #3
+        answersQUC['Development'] = getDevelopment(soup)            #4
 
-# 5. Corpo Docente
+        # 5. Corpo Docente
 
-print(answersQUC)
+        return answersQUC
 
+
+def main():
+    url = 'https://fenix.tecnico.ulisboa.pt/publico/viewCourseResults.do?executionCourseID=1690460473010771&degreeCurricularPlanOID=2581275345334'
+    answersQUC = getQUCdata(url)
+    #print(answersQUC)
+    print(answersQUC["Evaluation Method"])
+
+if __name__ == "__main__":
+    main()
